6cd3aa0d054af5eeceae15543d655fa45d6fa6f2
devmasf <55216227+devmasf@users.noreply.github.com>
2020-09-23 17:12:23 -0500

    Add new UI to Fuzzy search

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -7,15 +7,26 @@ $("#campground-search").on("input", function () {
     $("#campground-grid").html("");
     data.forEach(function (campground) {
       $("#campground-grid").append(`
-      <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-        <div class="card">
-          <img class="card-img-top" src="${campground.image} alt=${campground.name}">
-          <div class="card-body">
-            <h5 class="card-title">${campground.name}</h5>
-            <a href="/campgrounds/${campground._id}" class="btn btn-primary">More Info</a>
+        <div class="col-md-6 col-lg-4 mb-4" data-aos="fade-up" data-aos-delay="100">
+          <div class="block-campgrounds text-left">
+            <a href="#"><img class="img-responsive" src="${
+              campground.image
+            }" alt="${campground.name}"></a>
+            <div class="content-campgrounds">
+              <h4><a href="/campgrounds/${
+                campground._id
+              }">${campground.name}</a></h4>
+              <div class="description">
+                <p>${campground.description.substring(0, 100)}...</p>
+              </div>
+              <span><em>Submitted by <a href="/users/${
+                campground.author._id
+              }">${campground.author.username}</a></em></span>
+              <a class="pull-right readmore" href="/campgrounds/${
+                campground._id
+              }">read more</a>
+            </div>
           </div>
-        </div>
-      </div>
       `);
     });
   });
